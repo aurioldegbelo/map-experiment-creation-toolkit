d8f6e82948709c22216521d8da78759eca9a16b4
Max Gadomski <m.gadomski@conterra.de>
2024-07-09 20:29:25 +0200

    Create initial app structure and layout

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,87 @@
 <template>
-    <v-stepper :items="taskTitles">
-    </v-stepper>
+    <v-app>
+        <v-app-bar 
+            flat
+            :title="title"
+        ></v-app-bar>
+        <v-main>
+            <v-stepper
+                v-model="step"
+                alt-labels
+            >
+                <v-stepper-header>
+                    <template v-for="(title, index) in taskTitles">
+                        <v-stepper-item 
+                            :title="title"
+                            :value="index + 1"
+                        ></v-stepper-item>
+                    </template>
+                </v-stepper-header>
+                <v-stepper-window>
+                    <template v-for="(title, index) in taskTitles">
+                        <v-stepper-window-item :value="index + 1">
+                            <v-card :title="title">
+                                Lorem ipsum blablabla
+                            </v-card>
+                        </v-stepper-window-item>
+                    </template>
+                </v-stepper-window>
+            </v-stepper>
+        </v-main>
+        <v-footer app>
+            <v-stepper-actions></v-stepper-actions>
+        </v-footer>
+    </v-app>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Leaflet from 'leaflet';
+import data from '../data/experiments/UncertaintyViz/experiment.json';
 
-import 'leaflet/dist/leaflet.css';
-import experiment from '../data/experiments/UncertaintyViz/experiment.json';
-import data from '../data/experiments/UncertaintyViz/data/LakeCounty_Health_NationalObesityByState_2015.json';
-import type { Feature } from 'geojson';
+type Author = {
+    title?: string,
+    firstName: string,
+    lastName: string,
+    email: string
+}
 
-type Experiment = {
-    title: String,
-    description: String,
-    authors: String[],
-    tasks: Array<Task>
+enum WidgetType {
+    "text",
+    "input",
+    "map"
+}
+
+type Widget = {
+    type: string
+    properties: Object
 }
 
 type Task = {
-    title: String,
-    widgets: Object[]
+    title: string,
+    widgets: Widget[]
 }
 
-export default defineComponent({
+type Experiment = {
+    title: string,
+    description: string,
+    authors: Author[],
+    tasks: Task[]
+}
+
+export default {
     data() {
         return {
-            map: undefined,
             experiment: {} as Experiment,
-            taskTitles: [] as String[]
+            title: "" as string,
+            taskTitles: [] as string[],
+            step: 1 as number
         }
     },
-    computed: {
-    },
-    methods: {
-        setTaskTitles() {
-            const tasks = this.experiment?.tasks;
-
-            this.taskTitles = tasks.map(task => {
-                return task.title
-            });
-        }
-    },
+    computed: {},
+    methods: {},
     mounted() {
-        this.experiment = experiment;
-
-        this.setTaskTitles();
-
-        var map = Leaflet.map('map').setView([51.505, -0.09], 13);
-
-        const tiles = Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
-
-        console.log(experiment);
-
-        /* data: {
-            type: "FeatureCollection",
-            crs: {
-                type: "name",
-                properties: {
-                    name: "EPSG:4326"
-                }
-            },
-            features: [
-                {
-                    id: 0,
-                    type: "Feature",
-                    geometry: <Polygon>,
-                    properties: {
-                        OBJECTID: 0,
-                        NAME: "Texas",
-                        Obesity: 32.4
-                    }
-                }
-            ]
-        }
-        */
-
-        /* input parameters: {
-             classification method: 'equal interval', 'quantiles', 'natural breaks',
-             number of classes: <number>,
-             color scheme: 'sequential', 'diverging', 'qualitative'
-             variable: <String> e.g. "Obesity"
-         
-        } */
-
-        const features = data?.features;
-
-        const getColor = (value: number) => {
-            return value <= 25 ? 'green' : 'red';
-        }
-
-        const style = (feature: Feature) => {
-            return {
-                fillColor: getColor(feature?.properties?.Obesity)
-            }
-        };
-
-        Leaflet.geoJSON(data, { style: style }).addTo(map);
+        this.experiment = data;
+        this.title = this.experiment.title;
+        this.taskTitles = this.experiment.tasks.map(task => task.title);
     }
-});
+};
 </script>
