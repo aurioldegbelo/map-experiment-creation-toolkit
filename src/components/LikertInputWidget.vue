3fcea0b5302eed46474552ced435c7106cce0e22
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2024-07-23 15:55:01 +0200

    #7: Add likert widget

---
+++ src/components/LikertInputWidget.vue
@@ -0,0 +1,14 @@
+<template>
+</template>
+
+<script lang="ts">
+import type { PropType } from 'vue';
+
+export default {
+    props: {
+        options: {
+            type: Array as PropType<String[]>
+        }
+    }
+};
+</script>
