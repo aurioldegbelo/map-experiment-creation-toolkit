5efb8600c5b35c29875568e4ea46483b54bf1dca
Max Gadomski <46671467+m-gado01@users.noreply.github.com>
2024-08-19 23:51:34 +0200

    Refactoring

--- src/components/MapWidget.vue
+++ src/components/MapWidget.vue
@@ -4,24 +4,34 @@
 </template>
 
 <script lang="ts">
-import type { PropType } from 'vue';
 import type { ClassificationMethod } from '@/types/widgets/MapWidget';
-import type { Map, MapOptions, GeoJSONOptions, PathOptions, GeoJSON } from 'leaflet';
-import type { Feature, FeatureCollection } from 'geojson';
+import type { PropType } from 'vue';
+import type {
+    GeoJSONOptions,
+    Map,
+    MapOptions,
+    PathOptions
+} from 'leaflet';
+import type { FeatureCollection } from 'geojson';
 
 import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-
-import { equalIntervalBuckets, jenksBuckets, quantileBuckets } from 'geobuckets';
+/* FIXME: Add declaration file for geobuckets. */
+import {
+    equalIntervalBuckets,
+    jenksBuckets,
+    quantileBuckets
+} from 'geobuckets';
 
 import colorbrewer from 'colorbrewer';
 
 type MapWidgetData = {
     id: string,
-    map: Map,
-    mapOptions: MapOptions,
+    map: Leaflet.Map,
+    mapOptions: Leaflet.MapOptions,
     mapData: FeatureCollection,
-    classes: number[]
+    classes: number[],
+    dataLayer: Leaflet.FeatureGroup
 }
 
 export default {
@@ -50,27 +60,30 @@ export default {
     },
     data(): MapWidgetData {
         return {
-            id: `map_${Date.now()}`,
-            map: {} as Map,
+            id: `map_${Date.now()}`, //Generate unique map id
+            map: {} as Leaflet.Map,
             mapOptions: {
                 zoomSnap: 0
-            } as MapOptions,
+            } as Leaflet.MapOptions,
             mapData: {} as FeatureCollection,
-            classes: [] as number[]
+            classes: [] as number[],
+            dataLayer: {} as Leaflet.FeatureGroup
         }
     },
     computed: {
-        /* TODO: Use the url for number of classes instead of parameter */
+        numberOfClasses(): number {
+            const url = new URL(this.colorScheme);
+            const numberOfClasses = Number(url.searchParams.get("n"));
+            return numberOfClasses;
+        },
         colorPalette(): string {
             const url = new URL(this.colorScheme);
             const scheme = url.searchParams.get("scheme");
             /* FIXME: Fix TypeScript */
             return colorbrewer[scheme][this.numberOfClasses];
         },
-        numberOfClasses(): number {
-            const url = new URL(this.colorScheme);
-            const numberOfClasses = Number(url.searchParams.get("n"));
-            return numberOfClasses;
+        mapDataValues(): number[] {
+            return this.mapData.features.map(feature => feature?.properties && feature.properties[this.variableName]);
         }
     },
     mounted() {
@@ -78,55 +91,16 @@ export default {
     },
     methods: {
         async _initMap() {
-            this.map = Leaflet.map(this.id, { zoomSnap: this.mapOptions.zoomSnap });
-            //this.map.setView([51.505, -0.09], 13);
-            
-            //TODO: Configure basemaps via leaflet-providers (https://leaflet-extras.github.io/leaflet-providers/preview/index.html)
-            const basemapLayer = Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-            });
-
-            this.map.addLayer(basemapLayer);
-
-            //FIXME: Ensure input data follows GeoJSON spec
-            const dataSource = `/data/experiments/${import.meta.env.VITE_EXPERIMENT_ID}/src/${this.data}`;
-            this.mapData = await (await fetch(dataSource)).json();
-
-            const values: number[] = this.mapData.features.map(feature => feature.properties[this.variableName]);
-            const classificationMethod = this.classificationMethod.toString();
-            let buckets = [] as number[];
-
-            if (classificationMethod === "equal interval") {
-                buckets = await equalIntervalBuckets(values, this.numberOfClasses);
-            }
-
-            if (classificationMethod === "quantiles") {
-                buckets = await quantileBuckets(values, this.numberOfClasses);
-            }
-
-            if (classificationMethod === "jenks") {
-                buckets = await jenksBuckets(values, this.numberOfClasses);
-            }
-
-            const layerOptions: GeoJSONOptions = {
-                style: (feature): PathOptions => {
-                    return {
-                        fillColor: this._getColor(feature?.properties[this.variableName], buckets),
-                        fillOpacity: 1,
-                        color: "black",
-                        weight: 0.5
-                    }
-                }
-            }
-
-            const dataLayer = Leaflet.geoJSON(this.mapData, layerOptions);
-            const bounds = dataLayer.getBounds();
-            this.map.addLayer(dataLayer);
-
+            this.map = await Leaflet.map(this.id, this.mapOptions);
+            await this._addBasemap();
+            this.mapData = await this._getData();
+            this.classes = await this._calculateClasses();
+            await this._addData();
+            const bounds = this.dataLayer.getBounds();
             this.map.fitBounds(bounds);
 
             /* FIXME: TypeScript */
-            dataLayer.eachLayer(layer  => {
+            this.dataLayer.eachLayer(layer  => {
                 layer.on({
                     "mouseover": () => {
                         layer.setStyle({
@@ -135,15 +109,64 @@ export default {
                         });
 
                         layer.bringToFront();
-
-                        dataLayer.openPopup();
                     },
                     "mouseout": event => {
-                        dataLayer.resetStyle();
-                        dataLayer.closePopup();
+                        this.dataLayer.resetStyle();
                     },
                 });
             });
+        },
+        _addBasemap() {
+            //TODO: Configure basemaps via leaflet-providers (https://leaflet-extras.github.io/leaflet-providers/preview/index.html)
+            const basemapLayer = Leaflet.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            });
+
+            this.map.addLayer(basemapLayer);
+        },
+        async _getData() {
+            //FIXME: Ensure input data follows GeoJSON spec
+            const dataSource = `/data/experiments/${import.meta.env.VITE_EXPERIMENT_ID}/src/${this.data}`;
+            const data = await (await fetch(dataSource)).json();
+            return data;
+        },
+        async _calculateClasses(): Promise<number[]> {
+            const classificationMethod = this.classificationMethod.toString();
+            let classes = null;
+
+            switch (classificationMethod) {
+                case "equal interval": {
+                    classes = equalIntervalBuckets && equalIntervalBuckets(this.mapDataValues, this.numberOfClasses);
+                    break;
+                }
+
+                case "jenks": {
+                    classes = jenksBuckets && jenksBuckets(this.mapDataValues, this.numberOfClasses);
+                    break;
+                }
+
+                case "quantiles": {
+                    classes = quantileBuckets && quantileBuckets(this.mapDataValues, this.numberOfClasses);
+                    break;
+                }
+            }
+
+            return classes;
+        },
+        _addData() {
+            const layerOptions: GeoJSONOptions = {
+                style: (feature): PathOptions => {
+                    return {
+                        fillColor: this._getColor(feature?.properties[this.variableName], this.classes),
+                        fillOpacity: 1,
+                        color: "black",
+                        weight: 0.5
+                    }
+                }
+            }
+
+            this.dataLayer = Leaflet.geoJSON(this.mapData, layerOptions);
+            this.map.addLayer(this.dataLayer);
         },
         /* FIXME */
         _getColor(value: number, buckets: number[]): string {
